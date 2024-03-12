2875f04b376c59a0113857af19d1f2df9a0cd8b1
laudkyle <kyleaby1@gmail.com>
2024-03-12 18:50:40 +0000

    completed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+    const[nav, setNav] = useState(false);
+    const handleNav = () =>{
+        setNav(!nav);
+    }
   return (
     <div className="flex justify-between items-center h-24 max-w-[1000px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">React.</h1>
-      <ul className="flex hidden">
+      <ul className={"hidden md:flex"}>
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ?<AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 border-r h-full w-[60%] border-r-gray-900 bg-[#000300]">
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] p-4 mt-4">React.</h1>
-        <ul className="uppercase">
-          <li className="p-4 border-b border-b-gray-600">Home</li>
-          <li className="p-4 border-b border-b-gray-600">Company</li>
-          <li className="p-4 border-b border-b-gray-600">Resources</li>
-          <li className="p-4 border-b border-b-gray-600">About</li>
+      <div className={nav ? "fixed left-[-100%] ease-in-out duration-500" :"fixed left-0 top-0 border-r h-full w-[60%] border-r-gray-900 bg-[#000300] ease-in-out duration-500"}>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
+        <ul className="uppercase p-4">
+          <li className="p-4 border-b border-b-gray-800">Home</li>
+          <li className="p-4 border-b border-b-gray-800">Company</li>
+          <li className="p-4 border-b border-b-gray-800">Resources</li>
+          <li className="p-4 border-b border-b-gray-800">About</li>
           <li className="p-4">Contact</li>
         </ul>
       </div>
