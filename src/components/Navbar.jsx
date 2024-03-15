8b06c0437efacba38deab5a5442001c029773930
laudkyle <kyleaby1@gmail.com>
2024-03-15 08:15:11 +0000

    some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,11 +22,11 @@ const Navbar = () => {
       <div className={nav ? "fixed left-[-100%] ease-in-out duration-500" :"fixed left-0 top-0 border-r h-full w-[60%] border-r-gray-900 bg-[#000300] ease-in-out duration-500"}>
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
         <ul className="uppercase p-4">
-          <li className="p-4 border-b border-b-gray-800">Home</li>
-          <li className="p-4 border-b border-b-gray-800">Company</li>
-          <li className="p-4 border-b border-b-gray-800">Resources</li>
-          <li className="p-4 border-b border-b-gray-800">About</li>
-          <li className="p-4">Contact</li>
+          <li className="p-3 border-b border-b-gray-800">Home</li>
+          <li className="p-3 border-b border-b-gray-800">Company</li>
+          <li className="p-3 border-b border-b-gray-800">Resources</li>
+          <li className="p-3 border-b border-b-gray-800">About</li>
+          <li className="p-3">Contact</li>
         </ul>
       </div>
     </div>
